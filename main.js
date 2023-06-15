c9e21de69fb878aa68d4a23d5f2c3be692b8453b
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2023-06-15 00:05:34 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,118 @@
+
+// 1- Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+
+// alert("Hello World!");
+
+
+// =========================================================================
+
+// 2- Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+
+// let firstNumber
+// let secondNumber
+
+// firstNumber = Number(prompt("Digite um número"))
+// secondNumber = Number(prompt("Digite um número"))
+// let sum = firstNumber + secondNumber
+
+// alert(`A Soma dos números é de ${sum}`)
+
+
+// =========================================================================
+
+// 3- Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+
+// let number = 5;
+
+// let type = typeof(number)
+
+// type == "number" ? alert("É um número") : alert("Não é um número")
+
+// =========================================================================
+
+// 4- Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+
+// let word = "5";
+
+// let type = typeof(word)
+
+// type == "string" ? alert("É uma string") : alert("Não é uma string")
+
+
+// =========================================================================
+
+// 5- Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+
+// let test = true;
+
+// let type = typeof(test)
+
+// type == "boolean" ? alert("É um booleano") : alert("Não é um booleano")
+
+// =========================================================================
+
+// 6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+
+// let firstNumber
+// let secondNumber
+
+// firstNumber = Number(prompt("Digite um número"))
+// secondNumber = Number(prompt("Digite um número"))
+// let subtraction = firstNumber - secondNumber
+
+// alert(`o resultado da subtração entre os números é de ${subtraction}`)
+
+// =========================================================================
+
+
+// 7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+
+// let firstNumber
+// let secondNumber
+
+// firstNumber = Number(prompt("Digite um número"))
+// secondNumber = Number(prompt("Digite um número"))
+// let multiplication = firstNumber * secondNumber
+
+// alert(`o resultado da multiplicação entre os números é de ${multiplication}`)
+
+// =========================================================================
+
+
+// 8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+
+// let firstNumber
+// let secondNumber
+
+// firstNumber = Number(prompt("Digite um número"))
+// secondNumber = Number(prompt("Digite um número"))
+// let division = firstNumber / secondNumber
+
+// alert(`o resultado da divisão entre os números é de ${division}`)
+
+
+// =========================================================================
+
+
+// 9. Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+
+
+// let firstNumber
+
+// firstNumber = Number(prompt("Digite um número"))
+// let remainder  = firstNumber % 2
+
+// remainder == 0 ? alert("É um número par") : alert("Não é um número par")
+
+
+// =========================================================================
+
+
+// 10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+
+let firstNumber
+
+firstNumber = Number(prompt("Digite um número"))
+let remainder  = firstNumber % 2
+
+remainder != 0 ? alert("É um número impar") : alert("Não é um número impar")
